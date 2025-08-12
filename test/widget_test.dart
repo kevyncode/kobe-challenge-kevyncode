9b6c6300f8a90fd177af0c60d4dc561b7b25d7b5
Kevyn Rodrigues <kevyncodes@gmail.com>
2025-08-11 23:22:56 -0300

    Refactor code structure for improved readability and maintainability

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:desafio_tecnico_kobe/main.dart';
+import 'package:kobe_and_morty_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
